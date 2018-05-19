239122cb5c244514fd094ef4c532079729424cd6
protitude <miles.france@gmail.com>
2018-05-19 09:52:55 -0600

    time tweaks - update title

--- servicenow-tps/content.js
+++ servicenow-tps/content.js
@@ -18,12 +18,18 @@
       return $(this).text().replace("BS", "WL");
     });
     $("span.ng-binding").text(function () {
-      return $(this).text().replace("MyTime", "TPS Report");
+      return $(this).text().replace("Pendulum", "TPS Report");
     });
     $("html.date-calendar body tr.no-records-found td").prepend("<img src='http://protitude.com/officespace.jpg' />");
-    $("input#mon1").val('8');
+    $("input#mon1").val('7.5');
     $("input#tue1").val('4');
-    $("input#wed1").val('8');
+    $("input#wed1").val('7.5');
+    $("input#mon2").val('.5');
+    $("input#wed2").val('.5');
+    $( "button.btn-md" ).click(function() {
+      $("input#mon2").val('.5');
+      $("input#wed2").val('.5');
+    });
   });
 }(jQuery));
 
